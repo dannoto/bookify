0aa7a0bf10ccb20296287c675b6c5395dc8e796f
Daniel Ribeiro <sdanrib2018@gmail.com>
2022-12-03 13:03:27 -0300

    .

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -16,6 +16,9 @@ class Checkout extends CI_Controller
         $this->load->model('user_model');
         $this->load->model('plan_model');
         $this->load->model('payments_model');
+        $this->load->config('stripe');
+		$this->load->library('stripe_lib');
+        $this->user_model->authControl();
 
         // Get user ID from current SESSION 
         $this->userID = $this->session->userdata('session_user')['id'];
@@ -26,7 +29,9 @@ class Checkout extends CI_Controller
 
         $plan_get = htmlspecialchars($id);
         $plan = $this->plan_model->getPlan($plan_get);
-        
+
+        $user_data = $this->user_model->getUserById($this->userID);
+
 
         if ($plan) {
 
@@ -52,13 +57,18 @@ class Checkout extends CI_Controller
                     'plan_amount' => 0,
                     'plan_amount_currency' => 'brl',
                     'plan_interval' => $plan['plan_type'],
-                    'plan_interval_count' =>"-",
+                    'plan_interval_count' => "-",
                     'plan_period_start' => date('Y-m-d H:i:s'),
                     'plan_period_end' => "-",
                     'payer_email' => $this->user_model->getUserById($this->session->userdata('session_user')['id'])['user_email'],
                     'created' => date('Y-m-d H:i:s'),
                     'status' => "active",
                 );
+
+                //Cancel current plan/subscriptions.
+                if ($user_data['user_plan'] != 0) {
+                    $this->cancelCurrentSubscription($user_data['user_subscription']);
+                }
 
                 // Add Payment
                 $this->payments_model->addPayment($subscripData);
@@ -68,12 +78,11 @@ class Checkout extends CI_Controller
 
                 // UpdateSubscription
                 $this->plan_model->updateUserSubscription($this->session->userdata('session_user')['id'], $subscription_id);
-               
+
                 //  UpdateUser Plan
                 if ($this->plan_model->updateUserPlan($this->session->userdata('session_user')['id'], $plan_id)) {
-                    redirect(base_url('planos/sucesso?id='.$subscription_id.''));
+                    redirect(base_url('planos/sucesso?id=' . $subscription_id . ''));
                 }
-
             } else {
 
 
@@ -83,21 +92,19 @@ class Checkout extends CI_Controller
                     $postData = $this->input->post();
 
                     // Make payment 
-                    $paymentID = $this->payment($postData, $plan_id);
+                    $paymentID = $this->payment($postData, $plan_id, $user_data);
 
                     // If payment successful 
                     if ($paymentID) {
                         // redirect('user/pagamento_status' . $paymentID);
-                        redirect(base_url('planos/sucesso?id='.$paymentID.''));
-
+                        redirect(base_url('planos/sucesso?id=' . $paymentID . ''));
                     } else {
                         $apiError = !empty($this->stripe_lib->api_error) ? ' (' . $this->stripe_lib->api_error . ')' : '';
                         $data['error_msg'] = $apiError;
-                       
-                        redirect(base_url('planos/falhou?message='.$data['error_msg']));
 
+                        redirect(base_url('planos/falhou?message=' . $data['error_msg']));
                     }
-                } 
+                }
 
                 // Get plans from the database 
                 $data['plan'] = $plan;
@@ -105,13 +112,12 @@ class Checkout extends CI_Controller
                 // Pass plans data to the list view 
                 $this->load->view('user/checkout', $data);
             }
-
         } else {
             redirect(base_url('planos'));
         }
     }
 
-    function payment($postData, $plan_id)
+    function payment($postData, $plan_id,$user_data)
     {
 
         // If post data is not empty 
@@ -181,6 +187,11 @@ class Checkout extends CI_Controller
                                 'status' => $status
                             );
 
+                            //Cancel current plan/subscriptions.
+                            if ($user_data['user_plan'] != 0) {
+                                $this->cancelCurrentSubscription($user_data['user_subscription']);
+                            }
+
                             // Add Payment
                             $this->payments_model->addPayment($subscripData);
 
@@ -189,7 +200,7 @@ class Checkout extends CI_Controller
 
                             // Update subscription id in the users table  
                             if ($subscription_id) {
-                               
+
                                 //Upddate Plan ; 
                                 $this->plan_model->updateUserPlan($this->session->userdata('session_user')['id'], $plan_id);
 
@@ -207,17 +218,48 @@ class Checkout extends CI_Controller
         return false;
     }
 
-    function payment_status($id)
+    function cancelCurrentSubscription($subscription_id)
     {
-        $data = array();
+        $response = array();
 
-        // Get subscription data from the database 
-        $subscription = $this->user_model->getSubscription($id);
+        $cancel_request = $this->stripe_lib->cancelSubscription($subscription_id);
 
-        // Pass subscription data to the view 
-        $data['subscription'] = $subscription;
-        $this->load->view('user/pagamento_status', $data);
+        if ($cancel_request) {
+
+
+            $user = $this->session->userdata('session_user');
+            $plan = $this->plan_model->getPlan($user['user_plan']);
+
+            if ($cancel_request['status'] == 'canceled') {
+
+                if ($this->plan_model->updateSubscribeStatus($cancel_request['id'], $user['id'], 'canceled')) {
+
+                    $this->email_model->canceledSubscribe($user, $plan, $cancel_request);
+                    $response =  array('status' => 'true', 'message' => 'Sua assinatura foi cancelada. Voce não será mais cobrado.');
+                } else {
+                    $response =  array('status' => 'false', 'message' => 'Houve um problema no cancelamento. Contate o suporte.');
+                }
+            } else {
+                $response =  array('status' => 'false', 'message' => 'Houve um problema no cancelamento. Contate o suporte.');
+            }
+        } else {
+            $response =  array('status' => 'false', 'message' => 'Houve um problema no cancelamento. Contate o suporte.');
+        }
+
+        // print_r(json_encode($response));
     }
+
+    // function payment_status($id)
+    // {
+    //     $data = array();
+
+    //     // Get subscription data from the database 
+    //     $subscription = $this->user_model->getSubscription($id);
+
+    //     // Pass subscription data to the view 
+    //     $data['subscription'] = $subscription;
+    //     $this->load->view('user/pagamento_status', $data);
+    // }
 }
 
 // class Checkout extends CI_Controller
