02a1f1f8f75ee9d9667169a7b3591d4177375c01
Daniel Ribeiro <sdanrib2018@gmail.com>
2022-12-04 16:45:15 -0300

    .

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -22,7 +22,7 @@ class admin_model extends CI_Model
 
     public function authLogin() {
 
-        $actual_link = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $actual_link = $_SERVER['REQUEST_URI'];
 
         echo $actual_link;
 
