4d6d5e5e9f2a7420fbf22f0aef6779c798b0794e
Daniel Ribeiro <sdanrib2018@gmail.com>
2022-12-05 14:28:00 -0300

    .

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -27,81 +27,81 @@ class email_model extends CI_Model
     public function sendRecovery($user)
     {
         // ok
+        print_r($user);
 
         //Create an instance; passing `true` enables exceptions
-        $mail = new PHPMailer(true);
+        // $mail = new PHPMailer(true);
 
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = $this->config_model->getConfigEmail()['email_host'];                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = $this->config_model->getConfigEmail()['email_user'];                     //SMTP username
-            $mail->Password   = $this->config_model->getConfigEmail()['email_password'];                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = $this->config_model->getConfigEmail()['email_port'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        // try {
+        //     //Server settings
+        //     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        //     $mail->isSMTP();                                            //Send using SMTP
+        //     $mail->Host       = $this->config_model->getConfigEmail()['email_host'];                     //Set the SMTP server to send through
+        //     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        //     $mail->Username   = $this->config_model->getConfigEmail()['email_user'];                     //SMTP username
+        //     $mail->Password   = $this->config_model->getConfigEmail()['email_password'];                               //SMTP password
+        //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        //     $mail->Port       = $this->config_model->getConfigEmail()['email_port'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            //Recipients
-            $mail->setFrom($this->config_model->getConfigEmail()['user_email'], 'Bookify');
-            $mail->addAddress($user['user_email'], $user['user_name']." ".$user['user_surname']);     //Add a recipient
+        //     //Recipients
+        //     $mail->setFrom($this->config_model->getConfigEmail()['user_email'], 'Bookify');
+        //     $mail->addAddress($user['user_email'], $user['user_name']." ".$user['user_surname']);     //Add a recipient
 
 
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Recuperação de Senha';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //     //Content
+        //     $mail->isHTML(true);                                  //Set email format to HTML
+        //     $mail->Subject = 'Recuperação de Senha';
+        //     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        //     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-            if ($mail->send()) {
-                return true;
-                echo 'Message has been sent';
-            } else {
-                echo "envio falhou";
-            }
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+        //     if ($mail->send()) {
+        //         return true;
+        //         echo 'Message has been sent';
+        //     } else {
+        //         echo "envio falhou";
+        //     }
+        // } catch (Exception $e) {
+        //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        // }
     }
 
 
     public function welcome($user)
     {
-        print_r($user);
          //Create an instance; passing `true` enables exceptions
-        //  $mail = new PHPMailer(true);
+         $mail = new PHPMailer(true);
 
-        //  try {
-        //      //Server settings
-        //      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        //      $mail->isSMTP();                                            //Send using SMTP
-        //      $mail->Host       = $this->config_model->getConfigEmail()['email_host'];                     //Set the SMTP server to send through
-        //      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        //      $mail->Username   = $this->config_model->getConfigEmail()['email_user'];                     //SMTP username
-        //      $mail->Password   = $this->config_model->getConfigEmail()['email_password'];                               //SMTP password
-        //      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        //      $mail->Port       = $this->config_model->getConfigEmail()['email_port'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+         try {
+             //Server settings
+             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+             $mail->isSMTP();                                            //Send using SMTP
+             $mail->Host       = $this->config_model->getConfigEmail()['email_host'];                     //Set the SMTP server to send through
+             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+             $mail->Username   = $this->config_model->getConfigEmail()['email_user'];                     //SMTP username
+             $mail->Password   = $this->config_model->getConfigEmail()['email_password'];                               //SMTP password
+             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+             $mail->Port       = $this->config_model->getConfigEmail()['email_port'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
  
-        //      //Recipients
-        //      $mail->setFrom($this->config_model->getConfigEmail()['user_email'], 'Bookify');
-        //      $mail->addAddress($user['user_email'], $user['user_name']." ".$user['user_surname']);     //Add a recipient
+             //Recipients
+             $mail->setFrom($this->config_model->getConfigEmail()['user_email'], 'Bookify');
+             $mail->addAddress($user['user_email'], $user['user_name']." ".$user['user_surname']);     //Add a recipient
  
  
-        //      //Content
-        //      $mail->isHTML(true);                                  //Set email format to HTML
-        //      $mail->Subject = 'Bem-vindo ao Bookify';
-        //      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        //      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+             //Content
+             $mail->isHTML(true);                                  //Set email format to HTML
+             $mail->Subject = 'Bem-vindo ao Bookify';
+             $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
  
-        //      if ($mail->send()) {
-        //          return true;
-        //          echo 'Message has been sent';
-        //      } else {
-        //          echo "envio falhou";
-        //      }
-        //  } catch (Exception $e) {
-        //      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        //  }
+             if ($mail->send()) {
+                 return true;
+                 echo 'Message has been sent';
+             } else {
+                 echo "envio falhou";
+             }
+         } catch (Exception $e) {
+             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+         }
 
     }
 
