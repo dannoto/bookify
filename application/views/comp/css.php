3544021e9d729453bbe4c3297473e706bc385b7c
Daniel Ribeiro <sdanrib2018@gmail.com>
2022-12-05 00:42:20 -0300

    .

--- application/views/comp/css.php
+++ application/views/comp/css.php
@@ -8,6 +8,23 @@
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/owlcarousel/owl.theme.default.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url() ?>assets/img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url() ?>assets/img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url() ?>assets/img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url() ?>assets/img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url() ?>assets/img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url() ?>assets/img/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url() ?>assets/img/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url() ?>assets/img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>assets/img/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url() ?>assets/img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url() ?>assets/img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/img/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url() ?>assets/img/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?= base_url() ?>assets//ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
     <style>
         /* ===== Scrollbar CSS ===== */
