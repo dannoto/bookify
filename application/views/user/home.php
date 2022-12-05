8c42d327fc7b461beda4bd54558ad7f71cf71d78
Daniel Ribeiro <sdanrib2018@gmail.com>
2022-12-04 23:36:42 -0300

    .

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -201,23 +201,10 @@
 
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
-        
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/638d532ddaff0e1306dae723/1gjg0pqi3';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
+        <?=$this->admin_model->getSupport()['support_code']?>
     </script>
     <!--End of Tawk.to Script-->
 
-    <script type="text/javascript">
-    </script>
 
     <script>
         $('.ebook-add-biblioteca').on('click', function(e) {
